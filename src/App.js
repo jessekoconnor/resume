1f5381c022df405f4a931f58638b57e9d7b1423f
Jesse O'Connor <jesse.oconnor@meltwater.com>
2016-12-25 15:15:30 -0500

    touching up for victoria

--- src/App.js
+++ src/App.js
@@ -14,11 +14,14 @@ class AppHeader extends Component {
             </head>
             <body>
                 <h1>Whats up VICTORIA!!</h1>
+                <img style={{height: "80px"}}
+                    src="http://45.media.tumblr.com/a631c73aedc13c5a40c76d033b2361ba/tumblr_nxx72tylTt1ukx2eco1_1280.gif"
+                />
             </body>
         </div>
-          <h1>I hope your having a merry christmas!</h1>
-          <h2>(and are laughing and drinking with your family right now!) </h2>
-          <h3>Heres a great meme for your day: </h3>
+          <h1>Hope your having a merry christmas!!!!</h1>
+          <h2>(and are laughing and drinking with your family!!) </h2>
+          <h3>Heres a great meme. Why? Just because. </h3>
           <img
               src="http://www.ldssmile.com/wp-content/uploads/2013/11/5_4217171.jpg"
           />
