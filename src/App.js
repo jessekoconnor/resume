67083500a6efd22d96b8f35840e33aefb3094643
Jesse O'Connor <jesse.oconnor@meltwater.com>
2016-12-25 15:03:21 -0500

    Updating for victoria to look at

--- src/App.js
+++ src/App.js
@@ -13,15 +13,14 @@ class AppHeader extends Component {
                 <title>Resume Site</title>
             </head>
             <body>
-                <h1>Whats up C-SCHWAB!!</h1>
+                <h1>Whats up VICTORIA!!</h1>
             </body>
         </div>
-        <p className="App-intro">
-            Check out my new resume site im working on! I just got it public facing!!
-        </p>
+          <h1>I hope your having a merry christmas!</h1>
+          <h2>(and are laughing and drinking with your family right now!) </h2>
+          <h3>Heres a great meme for your day: </h3>
           <img
-              src="https://s-media-cache-ak0.pinimg.com/originals/ce/f1/73/cef173f99e514a78eae54418c2086f40.jpg"
-              className="cow"
+              src="http://www.ldssmile.com/wp-content/uploads/2013/11/5_4217171.jpg"
           />
       </div>
     );
